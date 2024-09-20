599e23b80bb9ebdf68b0cdba8d6a35d8f4b7d637
Michael J. Miller <michael.miller@dialecticeng.com>
2024-09-20 09:12:44 -0500

    Angular initial install

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::any('/{any}', function() {
+    return view('frontend');
+})->where('any', '^(?!api).*$');
